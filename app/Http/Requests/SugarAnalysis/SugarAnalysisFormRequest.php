dd4295cc8cadecd85ed2ae0a7ec77f95a8e37d5c
toomelette <toomelette@gmail.com>
2019-07-10 14:20:16 +0800

    Services Result from String to Numeric

--- app/Http/Requests/SugarAnalysis/SugarAnalysisFormRequest.php
+++ app/Http/Requests/SugarAnalysis/SugarAnalysisFormRequest.php
@@ -22,14 +22,37 @@ class SugarAnalysisFormRequest extends FormRequest{
         return [
             
         	'week_ending'=>'required|date_format:"m/d/Y"',
+            'date_submitted'=>'required|date_format:"m/d/Y"',
         	'date_sampled'=>'required|date_format:"m/d/Y"',
-        	'date_submitted'=>'required|date_format:"m/d/Y"',
         	'date_analyzed_from'=>'required|date_format:"m/d/Y"',
             'date_analyzed_to'=>'required|date_format:"m/d/Y"',
             'quantity'=>'nullable|string|max:45',
             'code'=>'sometimes|nullable|string|max:45',
             'report_no'=>'sometimes|nullable|string|max:45',
             'source'=>'sometimes|nullable|string|max:255',
+
+            'SS1001'=>'sometimes|nullable|numeric|max:10000',
+            'SS1002'=>'sometimes|nullable|numeric|max:10000',
+            'SS1003'=>'sometimes|nullable|numeric|max:10000',
+            'SS1004'=>'sometimes|nullable|numeric|max:10000',
+            'SS1005'=>'sometimes|nullable|numeric|max:10000',
+            'SS1006'=>'sometimes|nullable|numeric|max:10000',
+            'SS1007'=>'sometimes|nullable|numeric|max:10000',
+            'SS1008'=>'sometimes|nullable|numeric|max:10000',
+            'SS1009'=>'sometimes|nullable|numeric|max:10000',
+            'SS1010'=>'sometimes|nullable|numeric|max:10000',
+            'SS1011'=>'sometimes|nullable|numeric|max:10000',
+            'SS1012'=>'sometimes|nullable|numeric|max:10000',
+            'SS1013'=>'sometimes|nullable|numeric|max:10000',
+            'SS1014'=>'sometimes|nullable|numeric|max:10000',
+            'SS1015'=>'sometimes|nullable|numeric|max:10000',
+            'SS1016'=>'sometimes|nullable|numeric|max:10000',
+            'SS1017'=>'sometimes|nullable|string|max:45',
+            'SS1018'=>'sometimes|nullable|numeric|max:10000',
+            'SS1019'=>'sometimes|nullable|numeric|max:10000',
+            'SS1020'=>'sometimes|nullable|numeric|max:10000',
+            'SS1021'=>'sometimes|nullable|numeric|max:10000',
+
 
         ];
     
